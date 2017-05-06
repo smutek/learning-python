c2e18c064599cd8bc1ec1b1e59ab6456f0f303af
Jimmy Smutek <jimmy@smutek.net>
2017-05-06 18:31:10 -0400

    Dungeon game, first shot

--- Treehouse/python_collections/dungeon_game.py
+++ Treehouse/python_collections/dungeon_game.py
@@ -1,4 +1,6 @@
 import random
+import tkinter
+
 
 # draw a grid
 
@@ -43,41 +45,90 @@ def get_locations():
 def move_player(player, move):
 
     # get player location
+    x = player[0]
+    y = player[1]
     # if move == LEFT, x-1
+    if move == "LEFT":
+        x = x - 1
     # if move == RIGHT, x+1
+    elif move == "RIGHT":
+        x = x + 1
     # if move == UP, y-1
+    elif move == "UP":
+        y = y - 1
     # if move == DOWN, y+1
+    elif move == "LEFT":
+        y = y + 1
+
+    player = x, y
+
     return player
 
 
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+
+    x = player[0]
+    y = player[1]
+
     # if player y == 0, they can not move up (already at top of map)
+    if y == 0:
+        moves.remove("UP")
     # if player y == 4, they can not move down (already at bottom of map)
+    elif y == 4:
+        moves.remove("DOWN")
     # if player x == 0, they can not move left (already at left edge of map)
+    elif x == 0:
+        moves.remove("LEFT")
     # if player y == 4, they can not move right (already at right edge of map)
+    elif x == 4:
+        moves.remove("RIGHT")
+
     return moves
 
 
+locations = get_locations()
+
+monster = locations[0]
+door = locations[1]
+player = locations[2]
+
 while True:
+
+    available_moves = get_moves(player)
+
     print('Welcome to the dungeon!')
-    print("You're currently in room {}") # fill w/ player position
-    print("You can move {}") # fill with available moves
+    print("You're currently in room {}".format(player))  # fill w/ player position
+    print("You can move {}".format(available_moves))  # fill with available moves
     print("Enter QUIT to quit")
 
     move = input("> ")
     move = move.upper()
 
-    locations = get_locations()
-    print(locations)
+    print("door: {}".format(door))
+    print("monster: {}".format(monster))
 
     if move == 'QUIT':
         break
 
     # Good move? Change player position
-    # bad move? Don't change anything.
-    # On the door? They win.
-    # On the monster? They lose.
-    # Otherwise, loop again
+    if move in available_moves:
+        # move player
+        new_position = move_player(player, move)
+        player = new_position
 
+        # On the door? They win.
+        if player == door:
+            print("You win!")
+            break
+        # On the monster? They lose.
+        elif player == monster:
+            print("You got eaten you fuck head.")
+            break
+
+    # bad move? Don't change anything.
+    else:
+        print("bad move")
+
+    # Otherwise, loop again
 
