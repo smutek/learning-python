d016d86843dc44741c1305e4e3696e5196b32e64
Jimmy Smutek <jimmy@smutek.net>
2017-05-29 20:26:28 -0400

    protected

---
+++ Treehouse/unit-2/oo-python/protected.py
@@ -0,0 +1,23 @@
+class Protected:
+    __name = "Security"
+
+    def __method(self):
+        return self.__name
+
+class Circle:
+    def __init__(self, diameter):
+        self.diameter = diameter
+
+    @property
+    def radius(self):
+        return self.diameter / 2
+
+    @radius.setter
+    def radius(self, radius):
+        self.diameter = radius * 2
+
+small = Circle(10)
+print(small.diameter)
+print(small.radius)
+small.radius = 10
+print(small.diameter)
